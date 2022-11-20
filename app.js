52435a7dab708572f314ee668134ee18f1e1e239
Santosh Narayanan <santosh33@gmail.com>
2022-11-20 13:53:44 +0100

    Return 404 Page

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -14,7 +15,7 @@ app.use(shopRoutes);
 
 //handling page not found
 app.use((req, res, next) => {
-    res.status(404).send('<h1>Page not found</h1>');
+    res.status(404).sendFile(path.join(__dirname,'views','404.html'));
 });
 
 app.listen(3000);
