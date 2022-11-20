1e174455cc96d27e44ac6117767adde5d4780855
Santosh Narayanan <santosh33@gmail.com>
2022-11-20 12:39:24 +0100

    Router using express

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
-const http = require('http');
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-const server = http.createServer(app);
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-server.listen(3000);
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(adminRoutes);
+app.use(shopRoutes);
+
+//#region  code commented as it is handled by express.Router()
+/*app.use('/add-product', (req, res, next) => {
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+});
+//filtering request based on method
+app.post('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+});*/
+// #end region
+
+app.use('/', (req, res, next) => {
+    res.send('<h1>Hello from Express!</h1>');
+});
+
+app.listen(3000);
