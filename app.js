388dbe32c604d83e5d99a1d8436a55eb77764706
Santosh Narayanan <santosh33@gmail.com>
2022-11-20 12:50:04 +0100

    404 Error

--- app.js
+++ app.js
@@ -11,8 +11,9 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(adminRoutes);
 app.use(shopRoutes);
 
-app.use('/', (req, res, next) => {
-    res.send('<h1>Hello from Express!</h1>');
+//handling page not found
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not found</h1>');
 });
 
 app.listen(3000);
