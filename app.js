c22b3a57a51b99e2f888bdef96342581734aa312
Santosh Narayanan <santosh33@gmail.com>
2022-11-20 22:18:44 +0100

    Serving files statically

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname,'public')));
 
 //use filtering '/admin'
 app.use('/admin',adminRoutes);
