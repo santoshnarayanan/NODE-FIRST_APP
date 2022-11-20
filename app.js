b89fa103f07752d055a6f6f01ba8de940ba4c518
Santosh Narayanan <santosh33@gmail.com>
2022-11-20 12:43:52 +0100

    Changes done

--- app.js
+++ app.js
@@ -11,17 +11,6 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(adminRoutes);
 app.use(shopRoutes);
 
-//#region  code commented as it is handled by express.Router()
-/*app.use('/add-product', (req, res, next) => {
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
-});
-//filtering request based on method
-app.post('/product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-});*/
-// #end region
-
 app.use('/', (req, res, next) => {
     res.send('<h1>Hello from Express!</h1>');
 });
