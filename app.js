d285cdbaa25bd1ecbb10cf6e174c68cf18ca77f0
Santosh Narayanan <santosh33@gmail.com>
2022-11-24 23:07:32 +0100

    Finishing Controllers

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
+const errorController =  require('./controllers/error');
 
 const app = express();
 
@@ -19,8 +20,6 @@ app.use('/admin',adminRoutes);
 app.use(shopRoutes);
 
 //handling page not found
-app.use((req, res, next) => {
-    res.status(404).render('404',{pageTitle:'Page Not Found'});
-});
+app.use(errorController.get404);
 
 app.listen(3000);
