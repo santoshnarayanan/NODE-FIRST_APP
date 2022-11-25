7a93c5f3db1eeaceed9699c43d10a7aef6b22d17
Santosh Narayanan <santosh33@gmail.com>
2022-11-25 23:54:09 +0100

    Refactor code

--- models/product.js
+++ models/product.js
@@ -1,6 +1,21 @@
 const fs = require('fs');
 const path = require('path');
 
+const p = path.join(path.dirname(process.mainModule.filename),
+        'data',
+        'products.json');
+
+//#region helper function
+const getProductsFromFile = (cb)=>{
+        fs.readFile(p,(err, fileContent)=>{
+            if(err){
+               return  cb([]);
+            }
+            cb(JSON.parse(fileContent));
+        });
+}
+//#endregion
+
 const products =[];
 
 module.exports = class Product{
@@ -9,14 +24,7 @@ module.exports = class Product{
     }
 
     save(){
-        const p = path.join(path.dirname(process.mainModule.filename),
-        'data',
-        'products.json');
-        fs.readFile(p,(err,fileContent)=>{
-            let products = [];
-            if(!err){
-                products = JSON.parse(fileContent);
-            }
+        getProductsFromFile(products =>{
             products.push(this);
             fs.writeFile(p,JSON.stringify(products),(err)=>{
                 console.log(err);
@@ -25,14 +33,6 @@ module.exports = class Product{
     }
 
     static fetchAll(cb){
-        const p = path.join(path.dirname(process.mainModule.filename),
-        'data',
-        'products.json');
-        fs.readFile(p,(err, fileContent)=>{
-            if(err){
-                cb([]);
-            }
-            cb(JSON.parse(fileContent));
-        });
+        getProductsFromFile(cb);
     }
 }
