4462701a10d5f36b6bc85cf230a8d5a9c9bf3989
Santosh Narayanan <santosh33@gmail.com>
2022-11-26 00:46:29 +0100

    Design Shopping Structure Application

--- controllers/products.js
+++ controllers/products.js
@@ -1,7 +1,7 @@
 const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
-    res.render('add-product', {
+    res.render('admin/add-product', {
         pageTitle: 'Add Product',
         path: '/admin/add-product'
     });
@@ -17,7 +17,7 @@ exports.postAddProduct = (req, res, next) => {
 exports.getProducts = (req, res, next) => {
     const products = Product.fetchAll((products)=>{
     //render default template engine
-        res.render('shop',{prods:products, 
+        res.render('shop/product-list',{prods:products, 
             pageTitle:'Shop', path: '/'});
     });
     
