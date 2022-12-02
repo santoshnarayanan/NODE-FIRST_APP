9121a63517c5f2b77f1752b4e0b2ad0634f98a0e
Santosh Narayanan <santosh33@gmail.com>
2022-12-03 00:43:25 +0100

    Fetch single product from DB

--- models/product.js
+++ models/product.js
@@ -29,7 +29,7 @@ module.exports = class Product {
 
     //Loading product detail
     static findById(id) {
-
+        return db.execute('SELECT * from products WHERE products.id = ?',[id]);
     }
 
 };
