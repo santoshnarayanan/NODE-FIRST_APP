437f21902cfc1df804fc3986cf0b727aa643d831
Santosh Narayanan <santosh33@gmail.com>
2022-12-04 23:03:12 +0100

    Updating products

--- controllers/admin.js
+++ controllers/admin.js
@@ -30,17 +30,20 @@ exports.getEditProduct = (req, res, next) => {
         res.redirect('/');
     }
     const prodId = req.params.productId;
-    Product.findById(prodId, product => {
-        if (!product) {
-            res.redirect('/');
-        }
-        res.render('admin/edit-product', {
-            pageTitle: 'Edit Product',
-            path: '/admin/edit-product',
-            editing: editMode,
-            product: product
-        });
-    });
+
+    Product.findByPk(prodId)
+        .then(product => {
+            if (!product) {
+                res.redirect('/');
+            }
+            res.render('admin/edit-product', {
+                pageTitle: 'Edit Product',
+                path: '/admin/edit-product',
+                editing: editMode,
+                product: product
+            });
+        })
+        .catch(err => console.log(err));
 };
 
 exports.postEditProduct = (req, res, next) => {
@@ -49,11 +52,18 @@ exports.postEditProduct = (req, res, next) => {
     const updatedPrice = req.body.price;
     const updatedImageUrl = req.body.imageUrl;
     const updatedDescription = req.body.description;
-    const updatedProduct = new Product(prodId, updatedTitle, updatedImageUrl,
-        updatedDescription, updatedPrice);
-    updatedProduct.save();
-    res.redirect('/admin/products');
-
+    Product.findByPk(prodId)
+    .then(product=>{
+        product.title = updatedTitle;
+        product.price = updatedPrice;
+        product.description = updatedDescription;
+        product.imageUrl = updatedImageUrl;
+        product.save()
+        .then(result=>{console.log('Updated product successfully')})
+        .catch(err=>{console.log(err)});
+        res.redirect('/admin/products');
+    })
+    .catch(err=>console.log(err));
 };
 
 exports.getProducts = (req, res, next) => {
