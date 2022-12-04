fca8377e31dd5b511630364fc537f95a5649d1da
Santosh Narayanan <santosh33@gmail.com>
2022-12-04 23:27:55 +0100

    Delete products

--- controllers/admin.js
+++ controllers/admin.js
@@ -20,7 +20,10 @@ exports.postAddProduct = (req, res, next) => {
         imageUrl: imageUrl,
         description: description
     })
-        .then(result => { console.log('Created Product') })
+        .then(result => { 
+            console.log('Created Product');
+            res.redirect('/admin/products');
+        })
         .catch(err => { console.log(err) });
 };
 
@@ -82,6 +85,13 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
     const prodId = req.body.productId;
-    Product.deleteById(prodId);
-    res.redirect('/admin/products');
+    Product.findByPk(prodId)
+    .then(product=>{
+        product.destroy();
+    })
+    .then(result=>{
+        console.log('Product deleted successfully');
+        res.redirect('/admin/products');
+    })
+    .catch(err=>console.log(err));
 };
