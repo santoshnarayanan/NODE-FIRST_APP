fbaae14186968816e12c39c92582f840b0494c6b
Santosh Narayanan <santosh33@gmail.com>
2022-12-04 23:56:34 +0100

    Created Dummy User

--- app.js
+++ app.js
@@ -19,6 +19,15 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req,res,next)=>{
+    User.findByPk(1)
+    .then(user=>{
+        req.user = user;
+        next();
+    })
+    .catch(err=>console.log(err));
+});
+
 //use filtering '/admin'
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -33,9 +42,21 @@ Product.belongsTo(User,{
 
 User.hasMany(Product);
 
-sequelizeDb.sync({force:true})
+sequelizeDb
+    //.sync({force:true})
+    .sync()
     .then(result => {
+        return User.findByPk(1);
         //console.log(result);
+    })
+    .then(user=>{
+        if(!user){
+            return User.create({name:'Max', email:'test@test.com'});
+        }
+        return user;
+    })
+    .then(user=>{
+        //console.log(user);
         app.listen(3000);
     })
     .catch(err => { console.log(err) });
